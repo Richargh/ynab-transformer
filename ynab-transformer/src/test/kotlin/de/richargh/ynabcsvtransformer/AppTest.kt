faa61e82b3fcffd18d3d12c227464a3db4951290
Richard Gross <richard.gross@maibornwolff.de>
2021-09-04 13:06:14 +0200

    t: create a Test for the App

---
+++ ynab-transformer/src/test/kotlin/de/richargh/ynabcsvtransformer/AppTest.kt
@@ -0,0 +1,31 @@
+package de.richargh.ynabcsvtransformer
+
+import de.richargh.ynabcsvtransformer.lang.Res
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+
+class AppTest {
+
+    @Test
+    fun `should be able to read Db config plus csv and export in a different csv format`(){
+        // given
+        val testling = App()
+
+        // when
+        val result = ByteArrayOutputStream().use { stream ->
+            val configRes = javaClass.getResourceAsStream("app/db.config.json").use {
+                testling.readConfig(it)
+            }
+            javaClass.getResourceAsStream("app/db.csv").use {
+                testling.transform(it, (configRes as Res.Ok).value, stream.writer())
+            }
+            stream.toString()
+        }
+
+        // then
+        assertThat(result)
+                .contains("Date,Payee,Category,Memo,Outflow,Inflow")
+                .contains("2020-02-14,Anaconda,Entertainment: Gadget,Google,111-222222-3333333 Anaconda.de Notebook,-150.12,")
+    }
+}
