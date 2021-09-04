9d3bdafa02c1e0a28707dade24ddee6f81945e3a
Richard Gross <richard.gross@maibornwolff.de>
2021-09-04 11:01:38 +0200

    r: move the domain names to their own file

---
+++ ynab-transformer/src/main/kotlin/de/richargh/ynabcsvtransformer/input/DomainNames.kt
@@ -0,0 +1,45 @@
+package de.richargh.ynabcsvtransformer.input
+
+sealed class DomainName {
+    override fun toString(): String = javaClass.simpleName
+
+    object BookingDate : DomainName()
+    object Beneficiary : DomainName()
+    object Description : DomainName()
+    sealed class MoneyFlow : DomainName() {
+        sealed class PlusMinusFlow: MoneyFlow() {
+            object Flow: PlusMinusFlow()
+        }
+        sealed class InOutFlow: MoneyFlow() {
+            object InFlow: InOutFlow()
+            object OutFlow: InOutFlow()
+        }
+        sealed class MarkerFlow: MoneyFlow() {
+            object Flow: MarkerFlow()
+            class Marker(
+                    val inFlowMarker: String,
+                    val outFlowMarker: String): MarkerFlow(){
+
+                override fun equals(other: Any?): Boolean {
+                    if (this === other) return true
+                    return javaClass == other?.javaClass
+                }
+
+                override fun hashCode() = javaClass.hashCode()
+
+                companion object {
+                    fun any() = Marker("", "")
+                }
+            }
+        }
+    }
+
+
+    companion object {
+        val objects get() = DomainName::class.sealedSubclasses.mapNotNull { it.objectInstance }.toSet()
+        fun of(rawValue: String): DomainName {
+            return DomainName::class.sealedSubclasses.singleOrNull { it.simpleName == rawValue }?.objectInstance
+                    ?: throw RuntimeException("The key $rawValue does not exist in the domain.")
+        }
+    }
+}
